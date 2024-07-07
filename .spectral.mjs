2e8911530e8fc5c48c2e12c250b2a051043d7112
Prasang Misra <prasang@tread.io>
2024-07-07 16:40:41 -0400

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI1MTk3Mg/spectral.js?branch_slug=main&token=72fc85ad-bb74-4f03-a619-d891c158b37a";
+export default { extends: ruleset };
